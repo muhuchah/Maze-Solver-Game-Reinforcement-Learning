ebcdf57feaaabea8885f82b440744e3ffef77705
mohammadmx <mohammademazrouei@gmail.com>
2023-12-24 14:07:10 +0330

    change num episod

--- maze.py
+++ maze.py
@@ -10,6 +10,7 @@ GOAL = [9, 9]
 EPSILON = 0.1
 
 class RLQ:
+
     def __init__(self, discount_factor, learning_rate):
         self.Q = np.zeros((X, Y, 4))
         self.pi = np.zeros((X, Y), dtype=int)
@@ -19,15 +20,6 @@ class RLQ:
         self.action_reward = -0.001
         self.goal_reward = 1
 
-
-    def update_pi(self, state):
-        # action is index_max
-        action = max(range(len(self.Q[state[0], state[1]])), key=self.Q[state[0], state[1]].__getitem__)
-
-        self.pi[state[0], state[1]] = action
-
-    def policy(self, state):
-        return self.pi[state[0], state[1]]
 
     def update(self, state, next_state, action):
         if (next_state[0] == GOAL[0] and next_state[1] == GOAL[1]):
@@ -39,7 +31,19 @@ class RLQ:
         self.Q[state[0], state[1], action] = (1 - self.learning_rate) * state_q + self.learning_rate * (reward + self.gama*max(self.Q[next_state[0], next_state[1]]))
 
         self.update_pi(state)
+
+
+    def update_pi(self, state):
+        # action is index_max
+        action = max(range(len(self.Q[state[0], state[1]])), key=self.Q[state[0], state[1]].__getitem__)
+
+        self.pi[state[0], state[1]] = action
+
+
+    def policy(self, state):
+        return self.pi[state[0], state[1]]
     
+
     def print_pi(self):
         direction = {0: '^', 1: 'v', 2: '>', 3: '<'}
         for i in range(X):
@@ -49,6 +53,7 @@ class RLQ:
             print()
         print()
     
+
     def print_q(self):
         for i in range(X):
             for j in range(Y):
@@ -56,18 +61,22 @@ class RLQ:
                 ac = {0: '^', 1: 'v', 2: '>', 3: '<'}
                 print(round(max(self.Q[i, j]), 2), end=f'{ac[action]}\t')
             print()
+        print()
+
 
 # Create an environment
 env = gym.make("maze-random-10x10-plus-v0")
 observation = env.reset()
 
+
 state = START
 agent = RLQ(discount_factor=0.95, learning_rate=0.1)
 
 # Define the maximum number of iterations
-NUM_EPISODES = 1000000
+NUM_EPISODES_TRAIN = 500000
+NUM_EPISODES = 1000
 
-for episode in range(NUM_EPISODES):
+for episode in range(NUM_EPISODES_TRAIN):
     rnd = random.random()
     if rnd < EPSILON:
         action = random.randint(0, 3)
@@ -85,8 +94,9 @@ for episode in range(NUM_EPISODES):
         state = START
         EPSILON = 0.99 * EPSILON
         
+
 wins = 0
-for episode in range(1000):
+for episode in range(NUM_EPISODES):
     env.render()
 
     action = agent.policy(state)
@@ -106,7 +116,6 @@ for episode in range(1000):
 
 print(f"wins: {wins}")
 agent.print_pi()
-print()
 agent.print_q()
 time.sleep(60)
 
